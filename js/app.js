3d0ac2f76c76005c60d1aacd6958f3683eee194e
Kent C. Dodds <kent@doddsfamily.us>
2015-08-10 06:51:53 -0600

    👾 closures closures closures!

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 /*global app, $on */
+'use strict';
 require('todomvc-common/base.css');
 require('todomvc-app-css/index.css');
 require('./view');
@@ -7,31 +8,27 @@ require('./controller');
 require('./model');
 require('./store');
 require('./template');
-(function () {
-	'use strict';
+/**
+ * Sets up a brand new Todo list.
+ *
+ * @param {string} name The name of your new to do list.
+ */
+function Todo(name) {
+	this.storage = new app.Store(name);
+	this.model = new app.Model(this.storage);
+	this.template = new app.Template();
+	this.view = new app.View(this.template);
+	this.controller = new app.Controller(this.model, this.view);
+}
 
-	/**
-	 * Sets up a brand new Todo list.
-	 *
-	 * @param {string} name The name of your new to do list.
-	 */
-	function Todo(name) {
-		this.storage = new app.Store(name);
-		this.model = new app.Model(this.storage);
-		this.template = new app.Template();
-		this.view = new app.View(this.template);
-		this.controller = new app.Controller(this.model, this.view);
-	}
+var todo;
 
-	var todo;
+function setView() {
+	todo.controller.setView(document.location.hash);
+}
 
-	function setView() {
-		todo.controller.setView(document.location.hash);
-	}
-
-	$on(window, 'load', function() {
-		todo = new Todo('todos-vanillajs');
-		setView();
-	});
-	$on(window, 'hashchange', setView);
-})();
+$on(window, 'load', function () {
+	todo = new Todo('todos-vanillajs');
+	setView();
+});
+$on(window, 'hashchange', setView);
