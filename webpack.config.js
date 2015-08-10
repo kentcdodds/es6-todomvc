d19a4c151fffa89936a3305c8deb27aa2211646f
Kent C. Dodds <kent@doddsfamily.us>
2015-08-10 07:02:05 -0600

    reformat

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
 var path = require('path');
 
 module.exports = {
-  entry: './app.js',
-  output: {
-    filename: 'bundle.js',
-    path: here()
-  },
-  context: here('js'),
-  module: {
-    loaders: [
-      {test: /\.css$/, loaders: ['style', 'css']}
-    ]
-  }
+	entry: './app.js',
+	output: {
+		filename: 'bundle.js',
+		path: here()
+	},
+	context: here('js'),
+	module: {
+		loaders: [
+			{test: /\.css$/, loaders: ['style', 'css']}
+		]
+	}
 };
 
 function here(d) {
-  return d ? path.join(__dirname, d) : __dirname;
+	return d ? path.join(__dirname, d) : __dirname;
 }
