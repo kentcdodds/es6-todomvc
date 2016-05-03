5271eacf98b8217c3e95ad89a9ff58e5f9b45a21
Kent C. Dodds <kent@doddsfamily.us>
2016-05-03 07:24:09 -0600

    add facts loading

--- src/js/facts-loader.js
+++ src/js/facts-loader.js
@@ -4,5 +4,13 @@ const factsList = document.getElementById('facts-list')
 const factText = document.getElementById('fact-text')
 
 $on(factsList, 'click', ({target: {dataset: {fact}}}) => {
-  factText.innerText = fact
+  if (!fact) {
+    System.import('./facts/default-fact').then(setFactText)
+    return
+  }
+  System.import('./facts/' + fact).then(setFactText)
+
+  function setFactText({fact: animalFact}) {
+    factText.innerText = animalFact
+  }
 })
