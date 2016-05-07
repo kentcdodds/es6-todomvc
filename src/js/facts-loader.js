979edb562717eb2a65a631990a8e85dd9b4aa05e
Kent C. Dodds <kent@doddsfamily.us>
2016-05-06 19:45:32 -0600

    lazy load things

--- src/js/facts-loader.js
+++ src/js/facts-loader.js
@@ -1,14 +1,16 @@
-/* eslint import/namespace:0 */
 import {$on} from './helpers'
-import * as facts from './facts'
 
 const factsList = document.getElementById('facts-list')
 const factText = document.getElementById('fact-text')
 
 $on(factsList, 'click', ({target: {dataset: {fact}}}) => {
   if (!fact) {
-    factText.innerText = facts.defaultFact
+    System.import('./facts/default-fact').then(setFactText)
     return
   }
-  factText.innerText = facts[fact]
+  System.import('./facts/' + fact).then(setFactText)
+
+  function setFactText({fact: animalFact}) {
+    factText.innerText = animalFact
+  }
 })
