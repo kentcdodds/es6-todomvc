f51d5e77071f0daacb5f5976663951d5df2f1115
Kent C. Dodds <kent@doddsfamily.us>
2016-05-24 01:30:40 -0600

    add plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
+const webpack = require('webpack')
 const {resolve} = require('path')
 module.exports = env => {
+  const addPlugin = (add, plugin) => add ? plugin : undefined
+  const ifProd = plugin => addPlugin(env.prod, plugin)
+  const removeEmpty = array => array.filter(i => !!i)
   return {
     entry: './js/app.js',
     output: {
@@ -16,5 +20,28 @@ module.exports = env => {
         {test: /\.css$/, loader: 'style!css'},
       ],
     },
+    plugins: removeEmpty([
+      // doesn't save anything in this small app. npm@3 mostly takes care of this
+      ifProd(new webpack.optimize.DedupePlugin()),
+      // saves a couple of kBs
+      ifProd(new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false,
+        quiet: true,
+      })),
+      // saves 65 kB with Uglify!! Saves 38 kB without
+      ifProd(new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: '"production"',
+        },
+      })),
+      // saves 711 kB!!
+      ifProd(new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          screw_ie8: true, // eslint-disable-line
+          warnings: false,
+        },
+      })),
+    ])
   }
 }
