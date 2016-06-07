0c7622b4d63f168603b9f9bc4d2944019927c7d8
Kent C. Dodds <kent@doddsfamily.us>
2016-06-06 21:17:19 -0600

    WIP

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const {resolve} = require('path')
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = env => {
+  const addPlugin = (add, plugin) => add ? plugin : undefined
+  const ifProd = plugin => addPlugin(env.prod, plugin)
+  const removeEmpty = array => array.filter(i => !!i)
   return {
-    entry: './js/app.js',
+    entry: {
+      app: './js/app.js',
+      vendor: ['jquery', 'lodash'],
+    },
     output: {
       filename: 'bundle.[chunkhash].js',
       path: resolve(__dirname, 'dist'),
@@ -18,10 +25,14 @@ module.exports = env => {
         {test: /\.css$/, loader: 'style!css'},
       ],
     },
-    plugins: [
+    plugins: removeEmpty([
       new HtmlWebpackPlugin({
         template: './index.html'
-      })
-    ]
+      }),
+      ifProd(new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor'
+      }))
+    ]),
+    recordsPath: resolve(__dirname, '.webpack-records.json'),
   }
 }
