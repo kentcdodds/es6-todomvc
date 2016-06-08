0d1c07cf6209a6701fb90e6381ff30eb8b6f1097
Kent C. Dodds <kent@doddsfamily.us>
2016-06-07 21:11:09 -0600

    cover more lines

--- src/js/controller.test.js
+++ src/js/controller.test.js
@@ -1,7 +1,12 @@
+import getModelStub from './model.stub'
+import getViewStub from './view.stub'
 import Controller from './controller'
 
 describe('controller', () => {
-  it('exists', () => {
-    expect(Controller).to.exist
+  it('can be created', () => {
+    const view = getViewStub()
+    const model = getModelStub()
+    const controller = new Controller(model, view)
+    expect(controller).to.exist
   })
 })
