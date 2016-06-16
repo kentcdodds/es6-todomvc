fffee27f81115b57bb28db63ea8236dff7f7372d
Kent C. Dodds <kent@doddsfamily.us>
2016-06-16 03:05:25 -0600

    add coverage back to the project

--- karma.conf.js
+++ karma.conf.js
@@ -1,24 +1,27 @@
 const webpackConfig = require('./webpack.config')()
+process.env.BABEL_ENV = 'test' // so we load the correct babel plugins
 const testGlob = 'src/**/*.test.js'
+const srcGlob = 'src/**/*!(test|stub).js'
 
 module.exports = function setKarmaConfig(config) {
   config.set({
     basePath: '',
     frameworks: ['mocha', 'chai'],
-    files: [testGlob],
+    files: [testGlob, srcGlob],
     reporters: ['progress', 'coverage'],
     preprocessors: {
       [testGlob]: ['webpack'],
+      [srcGlob]: ['webpack'],
     },
     webpack: webpackConfig,
     webpackMiddleware: {noInfo: true},
-    // coverageReporter: {
-    //   reporters: [
-    //     {type: 'lcov', dir: 'coverage/', subdir: '.'},
-    //     {type: 'json', dir: 'coverage/', subdir: '.'},
-    //     {type: 'text-summary'},
-    //   ],
-    // },
+    coverageReporter: {
+      reporters: [
+        {type: 'lcov', dir: 'coverage/', subdir: '.'},
+        {type: 'json', dir: 'coverage/', subdir: '.'},
+        {type: 'text-summary'},
+      ],
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
