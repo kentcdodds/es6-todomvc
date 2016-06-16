b763401a4b93bb293c796f6b511173922668d9d8
Kent C. Dodds <kent@doddsfamily.us>
2016-06-16 02:28:41 -0600

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const {resolve} = require('path')
+const webpackValidator = require('webpack-validator')
+const {getIfUtils} = require('webpack-config-utils')
+
+module.exports = env => {
+  const {ifProd, ifNotProd} = getIfUtils(env)
+  return webpackValidator({
+    context: resolve('src'),
+    entry: './app.js',
+    output: {
+      filename: 'bundle.js',
+      path: resolve('dist'),
+      publicPath: '/dist/',
+      pathinfo: ifNotProd(),
+    },
+    devtool: ifProd('source-map', 'eval'),
+  })
+}
