0b5578e447bc9f000b93cfe1d9332e8380146e6c
Kent C. Dodds <kent@doddsfamily.us>
2016-06-21 21:15:34 -0500

    remove destructuring to support Node v4

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const {resolve} = require('path')
+const resolve = require('path').resolve
 const webpackValidator = require('webpack-validator')
 module.exports = () => {
   return webpackValidator({
