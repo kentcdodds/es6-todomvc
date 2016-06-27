b624f443fa8e0103796b9b60b95c9b1dc770eef3
Kent C. Dodds <kent@doddsfamily.us>
2016-06-27 07:32:09 -0600

    cover everything

--- karma.conf.js
+++ karma.conf.js
@@ -2,20 +2,31 @@ process.env.BABEL_ENV = 'test'
 const webpackEnv = {test: true}
 const webpackConfig = require('./webpack.config.babel')(webpackEnv)
 
-const fileGlob = 'src/**/*.test.js'
+const testGlob = 'src/**/*.test.js'
+const srcGlob = 'src/**/!(*.test|*.stub).js'
 
 module.exports = config => {
   config.set({
     basePath: '',
     frameworks: ['mocha', 'chai'],
-    files: [fileGlob],
+    files: [testGlob, srcGlob],
+    exclude: ['src/bootstrap.js'],
     preprocessors: {
-      [fileGlob]: ['webpack']
+      [testGlob]: ['webpack'],
+      [srcGlob]: ['webpack'],
     },
     webpack: webpackConfig,
     webpackMiddleware: {noInfo: true},
     reporters: ['progress', 'coverage'],
     coverageReporter: {
+      check: {
+        global: {
+          statements: 11,
+          branches: 0,
+          functions: 0,
+          lines: 11,
+        },
+      },
       reporters: [
         {type: 'lcov', dir: 'coverage/', subdir: '.'},
         {type: 'json', dir: 'coverage/', subdir: '.'},
