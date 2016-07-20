3ad6f3bbcdd335ee696bd44d9d63ac1ec53f02f9
Kent C. Dodds <kent@doddsfamily.us>
2016-07-19 21:42:01 -0600

    add HMR

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,5 +1,27 @@
+/* eslint no-console:0 */
 var app = require('./app')
 var helpers = require('./helpers')
 
-helpers.$on(window, 'load', app.onLoad)
-helpers.$on(window, 'hashchange', app.onLoad)
+// this is only relevant when using `hot` mode with webpack
+// special thanks to Eric Clemmons: https://github.com/ericclemmons/webpack-hot-server-example
+const reloading = document.readyState === 'complete'
+if (module.hot) {
+  module.hot.accept(function(err) {
+    console.log('❌  HMR Error:', err)
+  })
+  if (reloading) {
+    console.log('🔁  HMR Reloading.')
+    app.onLoad()
+  } else {
+    console.info('✅  HMR Enabled.')
+    bootstrap()
+  }
+} else {
+  console.info('❌  HMR Not Supported.')
+  bootstrap()
+}
+
+function bootstrap() {
+  helpers.$on(window, 'load', app.onLoad)
+  helpers.$on(window, 'hashchange', app.onLoad)
+}
